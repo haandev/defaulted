bea46d60b5dffa532c5ae9a936f9803bc178649a
HakanOzoglu <hakan.ozoglu@digital-art.de>
2021-01-06 11:54:26 +0300

    defaulted function

---
+++ index.js
@@ -0,0 +1,18 @@
+export function defaulted(obj, defaultObj) {
+    /** initializing return object */
+    let returnObj = {};
+    if (typeof obj === "undefined") { /** if source object is undefined, load defaults */
+        returnObj = defaultObj;
+    } else if (typeof obj !== typeof defaultObj) { /** if source objects type changed, it means changes are major, load source object */
+        returnObj = obj;
+    } else if (obj.constructor === Array) { /** if both of them an array, merge arrays */
+        returnObj = [...obj, ...defaultObj];
+    } else if (typeof obj === "object") { /** if objects are not array and they are objects, ( {etc..} , {etc..}) look up recursively */
+        for (const [key, value] of Object.entries(defaultObj)) {
+            returnObj[key] = this.normalize(obj[key], value);
+        }
+    } else { /** value changed, no-need to default value */
+        returnObj = obj;
+    }
+    return returnObj;
+}
